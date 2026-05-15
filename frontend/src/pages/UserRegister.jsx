e196fc673d31c3330773ae74aa7b772ad075c27e
Sakshamgoyal02 <sakshamgoyal142@gmail.com>
2026-05-16 02:58:41 +0530

    Fixed some api related issues

--- frontend/src/pages/UserRegister.jsx
+++ frontend/src/pages/UserRegister.jsx
@@ -17,7 +17,7 @@ const UserRegister = ()=>{
    const password = e.target.password.value;
   
 
-   const response = await api.post("/api/auth/user/register", {
+   const response = await api.post("/auth/user/register", {
     fullName: name,
     email,
     password
