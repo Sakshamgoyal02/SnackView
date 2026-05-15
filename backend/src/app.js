b8e96aed5288df61b86b56dc7f1fcaa1501b3174
Sakshamgoyal02 <sakshamgoyal142@gmail.com>
2026-05-16 01:47:10 +0530

    Fixed a deployment bug related to cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,24 +6,19 @@ const restaurantRoutes = require("./routes/restaurant.routes");
 const cors = require("cors");
 
 const app = express();
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://snack-view-frontend.vercel.app"
-];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      console.log("Blocked by CORS:", origin);
-      callback(new Error("CORS not allowed"));
-    }
-  },
-  credentials: true
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://snack-view-frontend.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-app.options("*", cors());
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 app.use(cookieParser());
