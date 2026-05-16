622c9a22e28e648651c7e8548b3faaf1851aadb6
Sakshamgoyal02 <sakshamgoyal142@gmail.com>
2026-05-16 07:27:32 +0530

    FIxed some bugs

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,18 +1,3 @@
-const app = require("../src/app");
-const connectDB = require("../src/db");
-
-let isConnected = false;
-
-module.exports = async (req, res) => {
-  try {
-    if (!isConnected) {
-      await connectDB();
-      isConnected = true;
-    }
-
-    return app(req, res);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Server error" });
-  }
-};a
+module.exports = (req, res) => {
+  res.status(200).json({ message: "API is working" });
+};
